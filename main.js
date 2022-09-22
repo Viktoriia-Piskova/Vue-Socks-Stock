caf5e23b177e57312b0736cbf1a8db29653991f8
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-09-22 15:34:31 +0300

    Events

--- main.js
+++ main.js
@@ -2,13 +2,29 @@ const product = 'Socks'
 const app = Vue.createApp({
     data(){
         return {
+            cart: 0,
             product: 'Socks',
             description: 'High quality',
             link: 'https://rozetka.com.ua/ua/',
             image: './assets/images/socks_blue.jpg',
             remain: 15, 
             details: ['50% cotton', '30% wool', '20% polyester'],
-            sizes: ['XS', 'S', 'M', 'L', 'XL']
+            sizes: ['XS', 'S', 'M', 'L', 'XL'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        removeFromCart() {
+            this.cart -= 1;
+        }
+
+
     }
 })
