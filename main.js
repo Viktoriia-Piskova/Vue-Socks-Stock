c55502e8ec82a54e840ee3159f07fda43c91f897
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-09-22 18:20:14 +0300

    Computed properties

--- main.js
+++ main.js
@@ -3,22 +3,23 @@ const app = Vue.createApp({
     data(){
         return {
             cart: 0,
+            selectedVariant: 0,
             product: 'Socks',
-            inStock: true,
             description: 'High quality',
             link: 'https://rozetka.com.ua/ua/',
-            image: './assets/images/socks_blue.jpg',
+            oldImage: './assets/images/socks_blue.jpg',
             remain: 5, 
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['XS', 'S', 'M', 'L', 'XL'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-                { id: 2235, color: 'red', image: './assets/images/socks_red.jpg' },
-                { id: 2235, color: 'orange', image: './assets/images/socks_orange.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, isNew: true },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 28, isNew: false},
+                { id: 2235, color: 'red', image: './assets/images/socks_red.jpg', quantity: 0, isNew: false},
+                { id: 2235, color: 'orange', image: './assets/images/socks_orange.jpg', quantity: 5, isNew: true }
               ]
         }
     },
+    
     methods: {
         addToCart() {
             this.cart += 1
@@ -28,10 +29,39 @@ const app = Vue.createApp({
             this.cart -= 1;
         },
 
-        updateImage(imageVariants) {
-            this.image = imageVariants
+        updateVariant(index) {
+            this.selectedVariant = index
         }
 
 
+    },
+
+    computed: {
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0 ? true : false
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+
+        onSale(){
+            if (this.variants[this.selectedVariant].quantity < 30 && this.variants[this.selectedVariant].quantity > 0){
+                return true
+            }
+        },
+
+        isLimited(){
+            if (this.variants[this.selectedVariant].quantity < 10 && this.variants[this.selectedVariant].quantity > 0){
+                return true
+            }
+        },
+
+        quantity(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        isNew(){
+            return this.variants[this.selectedVariant].isNew
+        }
     }
 })
