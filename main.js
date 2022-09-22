4ae636971da8506ce3b75552a31be196a64e7b7f
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-09-22 13:22:13 +0300

    Conditional and list renderings

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const app = Vue.createApp({
             link: 'https://rozetka.com.ua/ua/',
             image: './assets/images/socks_blue.jpg',
             remain: 15, 
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['XS', 'S', 'M', 'L', 'XL']
         }
     }
 })
